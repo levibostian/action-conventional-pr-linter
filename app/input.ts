becfda2cf204b97b8603bab99610e111896f4401
Levi Bostian <levi@levi.earth>
2022-01-27 15:52:25 -0600

    feat: give warning if change not allowed in branch

--- app/input.ts
+++ app/input.ts
@@ -1,12 +1,20 @@
 import { Input } from "./type/input"
 import * as core from "@actions/core"
 
-export const defaults: Input = {
-  token: ""
-}
-
 export const getInput = (): Input => {
-  return {
-    token: core.getInput("token")
+  const rawInput: Input = {
+    token: core.getInput("token"),
+    branchTypeWarning: JSON.parse(core.getInput("branchTypeWarning"))
   }
+
+  const branchTypeWarningObject: { [key: string]: unknown } = rawInput.branchTypeWarning || {}
+
+  // Try to filter out keys of object where value is not a string
+  rawInput.branchTypeWarning = Object.fromEntries(
+    Object.entries(branchTypeWarningObject).filter(
+      ([key]) => typeof branchTypeWarningObject[key] == "string"
+    )
+  ) as { [key: string]: string }
+
+  return rawInput
 }
--- app/type/input.ts
+++ app/type/input.ts
@@ -1,3 +1,4 @@
 export interface Input {
   token: string
+  branchTypeWarning: { [key: string]: string }
 }
