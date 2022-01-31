7e10f3a9419c072c43c98f8e8bde75d1ff9b32b5
Levi Bostian <levi@levi.earth>
2022-01-31 15:37:25 -0600

    find alternative bump method

--- app/lint.ts
+++ app/lint.ts
@@ -2,7 +2,6 @@ import lint from "@commitlint/lint"
 import { LintOptions, ParserOptions } from "@commitlint/types"
 import * as log from "./log"
 import { ReleaseType } from "./type/release_type"
-import commitAnalyzer from "@semantic-release/commit-analyzer"
 import parser from "conventional-commits-parser"
 import { Commit } from "conventional-commits-parser"
 import commitLintConventionalConfig from "@commitlint/config-conventional"
@@ -22,7 +21,7 @@ export const lintPrTitle = async (title: string): Promise<boolean> => {
   // In the past, I have used @commitlint/load to load rules + parserPresets
   // but was not able to get it working when using ncc.
   // https://github.com/levibostian/action-semantic-pr/blob/a2d16f81b2634c08c43a19d4dc92ad9c3a243d92/app/lint.ts#L19-L38
-  const preset = await presetLoader("conventionalcommits")
+  const preset = await (await presetLoader("conventionalcommits"))()
   const opts: LintOptions = {
     parserOpts: preset.parserOpts as ParserOptions
   }
@@ -33,23 +32,47 @@ export const lintPrTitle = async (title: string): Promise<boolean> => {
   return lintResult.valid
 }
 
-export const getNextReleaseType = async (prTitle: string): Promise<ReleaseType> => {
-  const result: ReleaseType = await commitAnalyzer.analyzeCommits(
-    {
-      preset: "conventionalcommits"
-    },
-    {
-      commits: [{ message: prTitle, hash: "XXX" }],
-      logger: console
-    }
-  )
-
-  return result
-}
-
 export const parseTitle = async (prTitle: string): Promise<Commit> => {
-  const preset = await presetLoader("conventionalcommits")
+  // for angular, it's more simple:
+  // require("conventional-changelog-angular")
+  // configuration = (await conventionalChangelogPresetLoader(`angular`)).parserOpts
+  // it just all depends on what is exported by the index.s for each module
+  const preset = await (await presetLoader("conventionalcommits"))()
   const rules = preset.parserOpts
 
   return parser.sync(prTitle, rules)
+}
+
+export const getNextReleaseType = async (prTitle: string): Promise<ReleaseType> => {
+  const preset = await (await presetLoader("conventionalcommits"))()
+  const bump = preset.recommendedBumpOpts.whatBump
+
+  const VERSIONS = ["major", "minor", "patch"]
+  // copied default rules for semantic-release. https://github.com/semantic-release/commit-analyzer/blob/16ea0f7389468bda69fefaf124310088ec746f0a/lib/default-release-rules.js
+  // in future, would be nice to have this config passed in or load from semantic-release
+  const releaseRules = [
+    { type: "feat", release: "minor" },
+    { type: "fix", release: "patch" },
+    { type: "perf", release: "patch" }
+  ]
+
+  const commit = await parseTitle(prTitle)
+
+  const result = bump([commit])
+  if (!result) return undefined
+
+  const recommendedLevel = VERSIONS[result.level]
+  if (recommendedLevel == "major") return recommendedLevel
+
+  // by default, bump() will return 'major' for feature and anything else is a patch.
+  // this might not match that of what's configured in your semantic release config.
+
+  let matched: ReleaseType = undefined
+  releaseRules.forEach((rule) => {
+    if (!matched && rule.type == commit.type) {
+      matched = rule.release as ReleaseType
+    }
+  })
+
+  return matched
 }
