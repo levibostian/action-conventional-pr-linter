0afa5e05fd4569eddfacb4fcc7a885e9d5dbd504
Levi Bostian <levi@levi.earth>
2022-01-31 15:50:41 -0600

    hard-code config to avoid hack

--- app/lint.ts
+++ app/lint.ts
@@ -1,14 +1,36 @@
 import lint from "@commitlint/lint"
-import { LintOptions, ParserOptions } from "@commitlint/types"
+import { LintOptions, ParserOptions, QualifiedRules } from "@commitlint/types"
 import * as log from "./log"
 import { ReleaseType } from "./type/release_type"
 import parser from "conventional-commits-parser"
-import { Commit } from "conventional-commits-parser"
+import { Commit, Options } from "conventional-commits-parser"
 import commitLintConventionalConfig from "@commitlint/config-conventional"
-import presetLoader from "conventional-changelog-preset-loader"
+import conventionCommitsSpec from "conventional-changelog-conventionalcommits"
 
-// import or it will not be bundled in compile js file
-import "conventional-changelog-conventionalcommits"
+interface Spec {
+  rules: QualifiedRules
+  parserOpts: Options
+  whatBump: Function
+}
+
+const getSpec = async (type: string): Promise<Spec> => {
+  if (type == "conventionalcommits") {
+    const spec = await (await conventionCommitsSpec())()
+    return {
+      rules: commitLintConventionalConfig.rules,
+      parserOpts: spec.parserOpts,
+      whatBump: spec.recommendedBumpOpts.whatBump
+    }
+    // } else if (type == "angular") {
+    //   const angular = await presetLoader("angular")
+    //   return {
+    //     parserOpts: angular.parserOpts,
+    //     whatBump: angular.recommendedBumpOpts.whatBump
+    //   }
+  } else {
+    throw Error("type unknown")
+  }
+}
 
 export const lintPrTitle = async (title: string): Promise<boolean> => {
   title = title.trim()
@@ -18,14 +40,11 @@ export const lintPrTitle = async (title: string): Promise<boolean> => {
 
   log.debug(`Linting PR ${title} with rules: ${rulesName}`)
 
-  // In the past, I have used @commitlint/load to load rules + parserPresets
-  // but was not able to get it working when using ncc.
-  // https://github.com/levibostian/action-semantic-pr/blob/a2d16f81b2634c08c43a19d4dc92ad9c3a243d92/app/lint.ts#L19-L38
-  const preset = await (await presetLoader("conventionalcommits"))()
+  const preset = await getSpec("conventionalcommits")
   const opts: LintOptions = {
     parserOpts: preset.parserOpts as ParserOptions
   }
-  const lintResult = await lint(title, commitLintConventionalConfig.rules, opts)
+  const lintResult = await lint(title, preset.rules, opts)
 
   log.debug(`Linting result: ${JSON.stringify(lintResult)}`)
 
@@ -37,15 +56,15 @@ export const parseTitle = async (prTitle: string): Promise<Commit> => {
   // require("conventional-changelog-angular")
   // configuration = (await conventionalChangelogPresetLoader(`angular`)).parserOpts
   // it just all depends on what is exported by the index.s for each module
-  const preset = await (await presetLoader("conventionalcommits"))()
+  const preset = await getSpec("conventionalcommits")
   const rules = preset.parserOpts
 
   return parser.sync(prTitle, rules)
 }
 
 export const getNextReleaseType = async (prTitle: string): Promise<ReleaseType> => {
-  const preset = await (await presetLoader("conventionalcommits"))()
-  const bump = preset.recommendedBumpOpts.whatBump
+  const preset = await getSpec("conventionalcommits")
+  const bump = preset.whatBump
 
   const VERSIONS = ["major", "minor", "patch"]
   // copied default rules for semantic-release. https://github.com/semantic-release/commit-analyzer/blob/16ea0f7389468bda69fefaf124310088ec746f0a/lib/default-release-rules.js
