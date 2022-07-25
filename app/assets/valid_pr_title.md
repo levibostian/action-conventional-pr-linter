d47a91705f9f124a25803cdcb7a0f9fcb6051a5a
Levi Bostian <levi@levi.earth>
2022-07-25 15:25:16 -0500

    feat: introduce footer to be more helpful for beginners

--- app/assets/valid_pr_title.md
+++ app/assets/valid_pr_title.md
@@ -1,5 +1,7 @@
-Pull request title looks good 👍. If this pull request gets merged, _it will {willCauseReleaseDescription} a new release_ of the software.
+**Pull request title looks good 👍!** 
 
-Example: If this project's latest release version is `1.0.0`. If this pull request gets merged in, the next release of this project will be `{nextReleaseExample}`.
+If this pull request gets merged, **it will {willCauseReleaseDescription} a new release** of the software. Example: If this project's latest release version is `1.0.0`. If this pull request gets merged in, the next release of this project will be `{nextReleaseExample}`.
+
+All merged pull requests will *eventually* get deployed. But some types of pull requests will trigger a deployment (such as features and bug fixes) while some pull requests will wait to get deployed until a later time. 
 
 **To merge this pull request, add the label `Ready to merge` to this pull request and I'll merge it for you.**
--- app/helper_messages.ts
+++ app/helper_messages.ts
@@ -2,10 +2,27 @@ import { readFileSync } from "fs"
 import path from "path"
 import stringFormat from "string-format"
 
-const getMessage = (args: object, relativeFilePath: string): string => {
-  const fileTemplate = readFileSync(path.join(__dirname, relativeFilePath), { encoding: "utf-8" })
+const getMessage = (
+  args: object,
+  relativeFilePath: string,
+  footerArgs: FooterArgs = { includedFooterDropdownOpened: "" }
+): string => {
+  const fileString = stringFormat(
+    readFileSync(path.join(__dirname, relativeFilePath), { encoding: "utf-8" }),
+    args
+  )
+  const footerString = stringFormat(
+    readFileSync(path.join(__dirname, "assets/footer.md"), { encoding: "utf-8" }),
+    footerArgs
+  )
+  const combinedStrings = fileString + "\n\n" + footerString
 
-  return stringFormat(fileTemplate, args)
+  return stringFormat(combinedStrings, args)
+}
+
+interface FooterArgs {
+  // If the footer markdown content should be expanded by default or not.
+  includedFooterDropdownOpened: "open" | ""
 }
 
 export interface InvalidPrTitleTemplateArgs {
@@ -18,7 +35,9 @@ export const getInvalidPrTitleHelp = (
 ): string => {
   const filePath = options?.filePath || "assets/invalid_pr_title_help.md"
 
-  return getMessage(args, filePath)
+  return getMessage(args, filePath, {
+    includedFooterDropdownOpened: "open"
+  })
 }
 
 export interface ValidPrTitleTemplateArgs {
